12b46cd258dbd076e73df5414a57419e43cee06f
Stas Strezh <37235564+StasStrezh@users.noreply.github.com>
2018-03-22 20:13:26 +0300

    Add files via upload

---
+++ ConvertingToOtherSystem/ConvertingToOtherSystem/Converting.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertingToOtherSystem
+{
+    public class Converting
+    {
+        /// <summary>
+        /// Converting numbers
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="num"></param>
+        public static double ConvertingNumbers(int p, string num)
+        {
+            if (p < 2 || p > 16)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if(num.Length<0 || num.Length>31)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return Convert(p, num);
+        }
+
+        /// <summary>
+        /// Converting method
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="num"></param>
+        /// <returns>Result</returns>
+        private static double Convert(int p, string num)
+        {
+            const string card = "0123456789ABCDEF";
+            double rank = 1, result = 0;
+            for (var i = num.Length - 1; i >= 0; i--)
+            {
+                var index = card.IndexOf(num[i]);
+                if (index < 0 || index >= p)
+                    throw new ArgumentException();
+
+                result += rank * index;
+                rank *= p;
+            }
+            return result;
+        }
+    }
+}
---
+++ ConvertingToOtherSystem/NUnitTestConverting/TestConverting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConvertingToOtherSystem;
+using NUnit.Framework;
+
+namespace ConvertingToOtherSystem
+{
+    [TestFixture]
+    public class TestConverting
+    {
+        [TestCase(2, "0110111101100001100001010111111", 934331071)]
+        [TestCase(2, "01101111011001100001010111111", 233620159)]
+        [TestCase(2, "11101101111011001100001010111111", 3991716543)]
+        [TestCase(16, "1AEF101", 28242177)]
+        [TestCase(16, "1ACB67", 1756007)]
+        [TestCase(8, "764241", 256161)]
+        public void TestConverting_ReturnResult(int p, string num, double expectedRes)
+        {
+            double res = Converting.ConvertingNumbers(p, num);
+            Assert.AreEqual(expectedRes, res);
+        }
+
+        [TestCase(2, "1AEF101")]
+        [TestCase(2, "SA123")]
+        public void TestConverting_ReturnExeption(int p, string num)
+        {
+            var ex = Assert.Catch<ArgumentException>(() => Converting.ConvertingNumbers(p, num));
+            StringAssert.Contains("Value does not fall within the expected range.", ex.Message);
+        }
+    }
+}
